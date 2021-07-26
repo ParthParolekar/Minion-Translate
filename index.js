d1147d79cbc19326b64d16fa617bf8dcf1a0618a
ParthParolekar <parolekarparth@gmail.com>
2021-07-26 22:53:31 +0530

    catch error

---
+++ index.js
@@ -0,0 +1,23 @@
+const input = document.querySelector("#text-area");
+const translateBtn = document.querySelector(".translate-btn");
+const translation = document.querySelector(".translation");
+let url = "https://api.funtranslations.com/translate/minion.json";
+
+translateBtn.addEventListener("click", translate);
+
+function serverUrl(text) {
+  return encodeURI(url + "?" + "text=" + text);
+}
+
+function translate() {
+  const inputText = input.value;
+
+  fetch(serverUrl(inputText))
+    .then((response) => response.json())
+    .then((data) => {
+      translation.innerText = data.contents.translated;
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
